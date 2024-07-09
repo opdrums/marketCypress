1e830cab83e552664a238a08fc4fa5f785fccca1
opdrums <operez6921@gmail.com>
2024-07-09 16:33:10 -0500

    page object model

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.spec.js',
     video:true,
     videoCompression:32,
+    reporter: 'mochawesome',
   },
   projectId: "vn4you"
 })
